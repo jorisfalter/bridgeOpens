62b1751479a61927f1f8a36bf1648df9fd2b7b16
Joris Falter <joris@jorisfalter.com>
2023-11-22 12:06:25 +0700

    x

--- downloadXmlProduction.py
+++ downloadXmlProduction.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import requests
 import gzip
+import io
 import xml.etree.ElementTree as ET
 import pymongo
 import certifi
@@ -8,6 +9,46 @@ from dotenv import load_dotenv
 import os
 import datetime
 import time
+
+
+def print_element_info(elem, indent=""):
+    text = elem.text.strip() if elem.text is not None else None
+    print(f"{indent}{elem.tag}: {elem.attrib}, Text: {text}")
+    for child in elem:
+        print_element_info(child, indent + "  ")
+
+# make data insertable into db
+
+
+def parse_element_info(elem):
+    # Create a dictionary to store element info
+    elem_dict = {
+        'tag': elem.tag,
+        'attributes': elem.attrib,
+        'text': elem.text.strip() if elem.text is not None else None,
+        'children': []
+    }
+
+    # Recursively parse child elements
+    for child in elem:
+        child_dict = parse_element_info(child)
+        elem_dict['children'].append(child_dict)
+
+    return elem_dict
+
+
+# initiate the DB
+ca = certifi.where()
+
+# Load environment variables from .env file
+load_dotenv()
+mongoPass = os.getenv("MONGO_ATLAS_PASS")
+
+client = pymongo.MongoClient(
+    f'mongodb+srv://joris:{mongoPass}@cluster0.igx7ca5.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=5000', tlsCAFile=ca)
+
+db = client['bridgeOpenDb']
+collection = db['bridgeOpenCollection']
 
 
 while True:
@@ -27,74 +68,36 @@ while True:
     else:
         print("Failed to download the file.")
 
-    # Specify the names of the input and output files
-    input_file = 'brugopeningen.xml.gz'
-    output_file = 'brugopeningen.xml'
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Decompress the content in memory
+        with gzip.open(io.BytesIO(response.content), 'rt', encoding='utf-8') as f_in:
+            xml_content = f_in.read()
 
-    # Open the gzip file in read mode and the output file in write mode
-    with gzip.open(input_file, 'rt', encoding='utf-8') as f_in:
-        xml_content = f_in.read()
+        # Process the XML content
+        # (Example: Parse the XML and do something with it)
+        root = ET.fromstring(xml_content)
 
-    print("Decompression complete.")
-    # print(xml_content)
+        # Define the substring you are looking for in the ID
+        # target_substring = "NLSPL002120533100119"
+        target_substring = "NLLID002060528100496"  # leiderdorpsebrug
 
-    # print data in console
+        # Find elements whose ID attribute contains the specific substring
+        for elem in root.iter():
+            id_value = elem.get('id')
+            if id_value and target_substring in id_value:
+                print("Found element with ID containing target substring:")
+                print_element_info(elem)
 
-    def print_element_info(elem, indent=""):
-        text = elem.text.strip() if elem.text is not None else None
-        print(f"{indent}{elem.tag}: {elem.attrib}, Text: {text}")
-        for child in elem:
-            print_element_info(child, indent + "  ")
+                # Parse the filtered element
+                dataBridge = parse_element_info(elem)
 
-    # make data insertable into db
+                # Insert into MongoDB
+                result = collection.insert_one(dataBridge)
+                break
 
-    def parse_element_info(elem):
-        # Create a dictionary to store element info
-        elem_dict = {
-            'tag': elem.tag,
-            'attributes': elem.attrib,
-            'text': elem.text.strip() if elem.text is not None else None,
-            'children': []
-        }
-
-        # Recursively parse child elements
-        for child in elem:
-            child_dict = parse_element_info(child)
-            elem_dict['children'].append(child_dict)
-
-        return elem_dict
-
-    # initiate the DB
-    ca = certifi.where()
-
-    # Load environment variables from .env file
-    load_dotenv()
-    mongoPass = os.getenv("MONGO_ATLAS_PASS")
-
-    client = pymongo.MongoClient(
-        f'mongodb+srv://joris:{mongoPass}@cluster0.igx7ca5.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=5000', tlsCAFile=ca)
-
-    db = client['bridgeOpenDb']
-    collection = db['bridgeOpenCollection']
-
-    # Parse the XML file
-    root = ET.fromstring(xml_content)
-
-    # Define the substring you are looking for in the ID
-    # target_substring = "NLSPL002120533100119"
-    target_substring = "NLLID002060528100496"  # leiderdorpsebrug
-
-    # Find elements whose ID attribute contains the specific substring
-    for elem in root.iter():
-        id_value = elem.get('id')
-        if id_value and target_substring in id_value:
-            print("Found element with ID containing target substring:")
-            print_element_info(elem)
-
-            # Parse the filtered element
-            dataBridge = parse_element_info(elem)
-
-            # Insert into MongoDB
-            result = collection.insert_one(dataBridge)
+        print("Processing completed successfully.")
+    else:
+        print("Failed to download the file.")
 
     time.sleep(60)  # Sleep for 60 seconds
