e2f133a93079609c6e3c775be8db296f826d1441
Joris Falter <joris@jorisfalter.com>
2023-12-12 11:17:48 +0700

    x

--- ledServerStatus.py
+++ ledServerStatus.py
@@ -1,21 +1,14 @@
 # this file is a test case of a server giving an indication
 
 from flask import Flask
+import time
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def test_ft():
     return 'ok', 200, {'Content-Type': 'text/plain'}
-
-
-@app.route('/ledstatus')
-def led_status():
-    # Use the function to determine the LED state
-    if get_led_state():
-        return 'on', 200, {'Content-Type': 'text/plain'}
-    else:
-        return 'off', 200, {'Content-Type': 'text/plain'}
 
 
 def get_led_state():
@@ -27,6 +20,15 @@ def get_led_state():
 
     # If the modulo is less than 5, the LED should be on
     return cycle_time < 5
+
+
+@app.route('/ledstatus')
+def led_status():
+    # Use the function to determine the LED state
+    if get_led_state():
+        return 'on', 200, {'Content-Type': 'text/plain'}
+    else:
+        return 'off', 200, {'Content-Type': 'text/plain'}
 
 
 if __name__ == '__main__':
