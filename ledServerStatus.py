7fee839b7d39e3f1d56c6f7be8ac3f75e584e62d
Joris Falter <joris@jorisfalter.com>
2023-12-13 07:35:46 +0700

    x

--- ledServerStatus.py
+++ ledServerStatus.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 import time
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -12,14 +14,46 @@ def test_ft():
 
 
 def get_led_state():
-    # Get the current time
-    current_time = time.time()
+    # # test case
+    # # Get the current time
+    # current_time = time.time()
 
-    # Find the time modulo 8 seconds (5 seconds on, 3 seconds off)
-    cycle_time = current_time % 8
+    # # Find the time modulo 8 seconds (5 seconds on, 3 seconds off)
+    # cycle_time = current_time % 8
 
-    # If the modulo is less than 5, the LED should be on
-    return cycle_time < 5
+    # # If the modulo is less than 5, the LED should be on
+    # return cycle_time < 5
+
+    # real case
+    # URL of the page to check
+    # Replace with the actual URL
+    # url = 'https://www.isdebrugopen.nl/zuid-holland/leiderdorp/leiderdorpsebrug'
+    url = 'https://www.isdebrugopen.nl/zeeland/terneuzen/oostsluis-binnenhoofd-brug'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the relevant section (adjust the selector as needed)
+        status_div = soup.find('div', class_='top-banner')
+
+        if status_div:
+            status_text = status_div.get_text(strip=True)
+
+            if "nee" in status_text.lower():
+                print("Nee")
+            elif "ja" in status_text.lower():
+                print("Ja")
+            else:
+                print("Status not found or unrecognized.")
+        else:
+            print("Status section not found in the HTML.")
+    else:
+        print("Failed to fetch the webpage.")
 
 
 @app.route('/ledstatus')
