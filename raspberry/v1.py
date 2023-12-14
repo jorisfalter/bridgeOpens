f85d9561619b0171d811dd1ac26753a6b5c271f4
Joris Falter <joris@jorisfalter.com>
2023-12-14 08:52:24 +0700

    x

--- raspberry/v1.py
+++ raspberry/v1.py
@@ -2,8 +2,12 @@ import machine
 import time
 
 led = machine.Pin("LED", machine.Pin.OUT)
+led_external = machine.Pin(0, machine.Pin.OUT)
+
 
 # Blink the onboard LED
 while True:
     led.toggle()  # Toggle the LED state between on and off
+    led_external.toggle()  # Set the LED to on
+
     time.sleep(1)  # Wait for 1 second
