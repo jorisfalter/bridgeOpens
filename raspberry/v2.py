102a4254534ca1cba7525e11f1265aa28f3a9bf9
Joris Falter <joris@jorisfalter.com>
2023-12-14 08:52:06 +0700

    x

--- raspberry/v2.py
+++ raspberry/v2.py
@@ -6,19 +6,19 @@ from machine import Pin
 wlan = network.WLAN(network.STA_IF)
 
 
-def connect_to_wifi(ssid, password, timeout=10):
+def connect_to_wifi(ssid, password, timeout=50):
     wlan.active(True)
 
-    if not wlan.isconnected():
+    while not wlan.isconnected():
         print("Connecting to Wi-Fi...")
         wlan.connect(ssid, password)
 
         # Wait for connection with a timeout
         start_time = time.ticks_ms()
         while not wlan.isconnected():
-            if time.ticks_diff(time.ticks_ms(), start_time) > timeout * 3000:
+            if time.ticks_diff(time.ticks_ms(), start_time) > timeout * 1000:
                 print("Connection timed out.")
-                return False
+                break
             time.sleep(1)
 
     print("Connected to Wi-Fi.")
@@ -36,10 +36,12 @@ connected = connect_to_wifi(ssid, password)
 if connected:
 
     led = machine.Pin("LED", machine.Pin.OUT)
+    led_external = machine.Pin(0, machine.Pin.OUT)
 
     # Blink the onboard LED
     while wlan.isconnected():  # check if the connection is made
         # while True:
         led.value(1)  # Set the LED to on
+        led_external.value(1)  # Set the LED to on
 
     print("Wi-Fi connection lost. Attempting to reconnect...")
