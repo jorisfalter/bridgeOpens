fcdfb1eae9b94e01d60ba16e51aa462f1e13d2c5
Joris Falter <joris@jorisfalter.com>
2024-12-26 17:01:13 +0100

    x

--- ledServerStatus.py
+++ ledServerStatus.py
@@ -67,15 +67,15 @@ def led_status():
   
  
     ## for testing purposes
-    return 'open', 200, {'Content-Type': 'text/plain'}
+    # return 'open', 200, {'Content-Type': 'text/plain'}
 
     ### Use the function to determine the LED state
     ### need to add additional error handling here based on the errors from the function
-    # gls_response = get_led_state()
-    # if gls_response:
-    #     return 'open', 200, {'Content-Type': 'text/plain'}
-    # else:
-    #     return 'gesloten', 200, {'Content-Type': 'text/plain'}
+    gls_response = get_led_state()
+    if gls_response:
+        return 'open', 200, {'Content-Type': 'text/plain'}
+    else:
+        return 'gesloten', 200, {'Content-Type': 'text/plain'}
 
 
 if __name__ == '__main__':
